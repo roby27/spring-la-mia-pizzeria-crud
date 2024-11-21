992ab2ee1d0131df13ae7afd64ef86a6b9e6b904
Roberto Branni <rob.branni@gmail.com>
2024-11-21 14:27:57 +0100

    form create e validazione

--- pizzeria/src/main/java/it/r27/pizzeria/controller/PizzaController.java
+++ pizzeria/src/main/java/it/r27/pizzeria/controller/PizzaController.java
@@ -6,13 +6,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.r27.pizzeria.model.Pizza;
 import it.r27.pizzeria.repository.PizzaRepository;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequestMapping("/pizzeria")
@@ -57,4 +63,23 @@ public class PizzaController {
 
 		return "pizzeria/show";
 	}
+
+	@GetMapping("/create")
+	public String create (Model model) {
+		model.addAttribute("pizza", new Pizza());
+		return "pizzeria/create";
+	}
+	
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "/pizzeria/create";
+		}
+
+		pizzaRepo.save(formPizza);
+		
+		return "redirect:/pizzeria";
+	}
+	
 }
--- pizzeria/src/main/java/it/r27/pizzeria/model/Pizza.java
+++ pizzeria/src/main/java/it/r27/pizzeria/model/Pizza.java
@@ -1,10 +1,15 @@
 package it.r27.pizzeria.model;
 
+import org.hibernate.validator.constraints.URL;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 @Table(name = "pizza")
@@ -14,12 +19,21 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+    @NotNull(message = "Il nome della pizza non è valido.")
+    @NotBlank(message = "Il nome della pizza non può essere vuoto.")
     private String name;
 
+    @NotNull(message = "La descrizione della pizza non è valida.")
+    @NotBlank(message = "La descrizione della pizza non può essere vuota.")
     private String description;
 
-    private double price;
+    @NotNull(message = "Devi inserire il prezzo.")
+    @Positive(message = "Il prezzo non può essere 0.")
+    private Double price;
 
+    @NotNull(message = "Il link non è valido.")
+    @NotBlank(message = "Il link non può essere vuoto.")
+    @URL(protocol = "https", message = "Non hai inserito un URL valido (usa solo link con https)")
     private String image;
 
     public void setId(Long id) {
@@ -34,7 +48,7 @@ public class Pizza {
         this.description = description;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
@@ -54,7 +68,11 @@ public class Pizza {
         return description;
     }
 
-    public String getPrice() {
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getFormattedPrice() {
         return String.format("%.2f €", price);
     }
 
